2aa438cd6953c8155792bb8b4be7a5693c144ff1
bardiya <Bardiya.bz@gmail.com>
2020-08-19 20:02:50 +0430

    some bugs fixed

--- database/migrations/2020_07_24_100003_create_educations_table.php
+++ database/migrations/2020_07_24_100003_create_educations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateQualificationsTable extends Migration
+class CreateEducationsTable extends Migration
 {
     /**
      * Run the migrations.
